5297d43337e143c0f15ec2fa210100759a109321
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-11 15:32:46 +0530

    add a new AppState class with copywith mathod

--- lib/slider_provider.dart
+++ lib/slider_provider.dart
@@ -1,7 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final sliderProvider = StateProvider<double>(
+final sliderProvider = StateProvider<AppState>(
   (ref) {
-    return 0.0;
+    return AppState(slider: 0.4, showPassword: false);
   },
 );
+
+class AppState {
+  final double slider;
+  final bool showPassword;
+  AppState({required this.slider, required this.showPassword});
+
+  AppState copyWith({double? slider, bool? showPassword}) {
+    return AppState(
+        slider: slider ?? this.slider,
+        showPassword: showPassword ?? this.showPassword);
+  }
+}
