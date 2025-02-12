6aa083f5bbf2c1b4485dbdc96c7191284ddc6db8
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-12 11:13:52 +0530

    perform a curd operation using riverpod

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,51 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_tut/search_provider.dart';
+import 'package:riverpod_tut/item_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final item = ref.watch(itemProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Text App'),
+        title: const Text('Riverpod App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              onChanged: (value) {
-                ref.read(searchProvider.notifier).search(value);
-              },
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Consumer(
-              builder: (context, ref, child) {
-                final search =
-                    ref.watch(searchProvider.select((state) => state.search));
-                return Text(
-                  search,
-                  style: const TextStyle(fontSize: 50),
+      body: item.isEmpty
+          ? const Center(child: Text('No data found'))
+          : ListView.builder(
+              itemCount: item.length,
+              itemBuilder: (context, index) {
+                final itemDetail = item[index];
+                return ListTile(
+                  title: Text(itemDetail.name),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          ref
+                              .read(itemProvider.notifier)
+                              .deleteItem(itemDetail.id);
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          ref
+                              .read(itemProvider.notifier)
+                              .updateItem(itemDetail.id, 'updated item');
+                        },
+                        icon: const Icon(Icons.edit),
+                      )
+                    ],
+                  ),
                 );
               },
             ),
-            Consumer(
-              builder: (context, ref, child) {
-                final search = ref.watch(searchProvider);
-                return Switch(
-                  value: search.isChange,
-                  onChanged: (value) {
-                    ref.read(searchProvider.notifier).onChange(value);
-                  },
-                );
-              },
-            ),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(itemProvider.notifier).addItem('hi item');
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
